b02a6f781428f0d3a19d6cc3d2e4eceacb5c8b9f
amelie <wuxiaojun55555@gmail.com>
2017-06-22 16:00:27 +0800

    实时统计

--- stock_analysis/realtime_get.py
+++ stock_analysis/realtime_get.py
@@ -1,14 +1,30 @@
 #coding: utf-8
-from datetime import datetime, timedelta
 import logging
+from datetime import datetime, timedelta
+
 import tushare as ts
-from stock_analysis.spider_all import get_zs_tonghuashun,get_tx_minite
 
 logging.basicConfig(level=logging.INFO,
                 format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                 datefmt='%a, %d %b %Y %H:%M:%S',
                 filename='zs.log',
                 filemode='a')
+
+def transfer(x):
+    ntype=''
+    if x >= 9.9:
+        ntype='10'
+    elif x>=5:
+        ntype='5-9'
+    elif x>=0:
+        ntype='0-5'
+    elif x>=-5:
+        ntype='-5-0'
+    elif x >= -9.9:
+        ntype = '-10- -5'
+    else:
+        ntype='-10'
+    return ntype
 
 def work():
     #get_tx_minite()
@@ -18,17 +34,23 @@ def work():
     print(df.head(30))
 
 
+def work_all():
+    df=ts.get_today_all()
+    df=df[['changepercent','trade','volume']]
+    logging.info(df.describe())
+    df['c'] = df.apply(lambda x: transfer(x['changepercent']), axis=1)
+    cnt = df.groupby(df['c']).size().rename('counts')
+    logging.info(cnt)
+
+
+
 
 
 def runTask(func, day=0, hour=0, min=0, second=0):
   # Init time
   now = datetime.now()
-  strnow = now.strftime('%Y-%m-%d %H:%M:%S')
-
-  # First next run time
   period = timedelta(days=day, hours=hour, minutes=min, seconds=second)
-  next_time = now + period
-  strnext_time = next_time.strftime('%Y-%m-%d %H:%M:%S')
+  strnext_time = now.strftime('%Y-%m-%d %H:%M:%S')
   while True:
       # Get system current time
       iter_now = datetime.now()
@@ -46,5 +68,5 @@ def runTask(func, day=0, hour=0, min=0, second=0):
           # Continue next iteration
           continue
 
-runTask(work, min=10/60)
+runTask(work_all, min=60/60)
 #runTask(work, day=1, hour=2, min=1)
