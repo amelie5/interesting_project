8caf4b191315651f0dd80b8686fe27ddedb01760
amelie <wuxiaojun55555@gmail.com>
2017-07-20 16:59:05 +0800

    缠论合并k线与分笔

---
+++ stock_analysis/chanlun_test.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine, Table, Column, MetaData, FLOAT, String, DATE, Integer, TIMESTAMP
+from chanlun_util import find_peak_and_bottom,KLineDTO,fen_bi
+
+code='603799'
+date='2017-04-01'
+k_line_list=[]
+
+# 连接数据库
+engine = create_engine('mysql+pymysql://root:wxj555@127.0.0.1/my_db?charset=utf8')
+conn = engine.connect()
+r=conn.execute('select date,high,low,open,close from price_amount where code=%s and date>=%s',code,date)
+res = r.fetchall()
+for x in res:
+    date_time = x[0]
+    open_price = x[3]
+    close_price = x[4]
+    high_price = x[1]
+    low_price = x[2]
+    k_line_dto = KLineDTO(date_time,date_time,date_time,open_price, high_price, low_price, close_price)
+    k_line_list.append(k_line_dto)
+
+merge_line_list = find_peak_and_bottom(k_line_list, "down")
+fenbi_result,final_result_array,fenbi_seq_list = fen_bi(merge_line_list)
